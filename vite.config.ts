c315947c69464ca3e9132cf84268bd6b6adffc50
Jimmy Jouanne <jimmy.jouanne@wttj.co>
2022-09-29 08:24:44 +0200

    feat: add path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /// <reference types="vitest" />
+/// <reference types="vite-plugin-svgr/client" />
 
 import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  plugins: [react(), svgr()],
   test: {
     globals: true,
     environment: 'jsdom',
